b3c010c2d28e45f04ebdca3abc60bd3f37a0f86b
Lanak <lanak-mu@mail.ru>
2020-10-09 20:10:06 +0300

    Scripted auto-commit on change (2020-10-09 20:10:06) by gitwatch.sh

--- src/main/java/WordReducer.java
+++ src/main/java/WordReducer.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.mapred.MapOutputCollector;
 import java.io.IOException;
 
 public class WordReducer extends Reducer<Display.Text, IntWritable, Display.Text, LongWritable> {
-    @Override
+    
     protected void reduce(Display.Text key, Iterable<IntWritable> values, MapOutputCollector.Context context) throws
             IOException, InterruptedException {
 
