0145d13873e195a874a17f4f6db9b50296c9c1ed
Lanak <lanak-mu@mail.ru>
2020-10-09 18:58:30 +0300

    Scripted auto-commit on change (2020-10-09 18:58:30) by gitwatch.sh

--- src/main/java/WordMapper.java
+++ src/main/java/WordMapper.java
@@ -1,2 +1,14 @@
-public class WordMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import javax.xml.soap.Text;
+import java.io.IOException;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
+
+    }
 }
--- src/main/java/WordReducer.java
+++ src/main/java/WordReducer.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class WordReducer {
+public class WordReducer {
 }
