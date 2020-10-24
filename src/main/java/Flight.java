46fed73d53955590b3fcb73f7412c7c95b664e9b
Lanak <lanak-mu@mail.ru>
2020-10-24 18:11:42 +0300

    Scripted auto-commit on change (2020-10-24 18:11:42) by gitwatch.sh

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,20 @@
+import java.util.Date;
+
 public class Flight {
+    private int year;
+    private int quarter;
+    private int month;
+    private int day_of_month;
+    private int day_of_week;
+    private Date fl_date;
+    private int unique_carrier;
+    private int airline_id;
+    private int carrier;
+    private int tail_num;
+    private int origin_airport_id;
+    private int origin_airport_seq_id;
+    private int origin_city_market_id;
+    private int dest_airport_id;
+    private int dest_aeroport_id;
+    private int whee
 }
