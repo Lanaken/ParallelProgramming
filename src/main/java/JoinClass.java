37ad1562fbcffe10a4d8cb1c5863a61d364c3690
Lanak <lanak-mu@mail.ru>
2020-10-25 21:12:55 +0300

    Scripted auto-commit on change (2020-10-25 21:12:55) by gitwatch.sh

--- src/main/java/JoinClass.java
+++ src/main/java/JoinClass.java
@@ -1,2 +1,13 @@
+
+import org.apache.log4j.BasicConfigurator;
+
+
 public class JoinClass {
+    public static void main(String[] args){
+        BasicConfigurator.configure();
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
+    }
 }
