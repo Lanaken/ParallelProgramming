885678789502edd04d94f0209f7f8ae2db199b44
Lanak <lanak-mu@mail.ru>
2020-11-14 15:35:40 +0300

    Scripted auto-commit on change (2020-11-14 15:35:40) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -1,5 +1,20 @@
+import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
 
-public class AirportWritable extends Writable {
+public class AirportWritable implements WritableComparable {
 
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
--- src/main/java/JoinClass.java
+++ src/main/java/JoinClass.java
@@ -1,15 +1,36 @@
 
-import org.apache.hadoop.mapred.JobConf;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.log4j.BasicConfigurator;
+
+import java.io.IOException;
 
 
 public class JoinClass {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         BasicConfigurator.configure();
-        if (args.length != 2) {
-            System.err.println("Usage: WordCountApp <input path> <output path>");
+        if (args.length != 3) {
+            System.err.println("Usage: WordCountApp <input path> <input path> <output path>");
             System.exit(-1);
         }
-        JobConf conf = new JobConf(JoinJob.clas);
-    }
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinClass.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+   }
 }
