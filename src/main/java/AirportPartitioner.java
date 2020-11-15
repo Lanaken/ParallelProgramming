e90e716046596a70195b7e47ad21ca66b2346897
Lanak <lanak-mu@mail.ru>
2020-11-16 01:21:28 +0300

    Scripted auto-commit on change (2020-11-16 01:21:28) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -1,2 +1,10 @@
-public class AirportPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirportPartitioner extends Partitioner<AirportWritable, Text> {
+
+    @Override
+    public int getPartition(AirportWritable airportWritable, Text text, int i) {
+        return 0;
+    }
 }
