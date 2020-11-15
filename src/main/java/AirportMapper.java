c99c30d7202b21c1d204bb66640bfc66ef456595
Lanak <lanak-mu@mail.ru>
2020-11-16 00:51:47 +0300

    Scripted auto-commit on change (2020-11-16 00:51:47) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -7,11 +7,12 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private static final int AIRPORT_ID = 0;
+    private static final int DESCRIPTION = 1;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
-        String[] words = line.replaceAll("[-.,:()!…—;?«»„]","").replace('[', ' ').replace(']', ' ')
-                .toLowerCase().split(" ");
+        String[] words = line.split(",")
         for (String word : words){
             context.write(new Text(word),new IntWritable(1));
         }
