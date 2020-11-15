e501bdee08851026be889937f37173fb6f99d891
Lanak <lanak-mu@mail.ru>
2020-11-16 01:31:01 +0300

    Scripted auto-commit on change (2020-11-16 01:31:01) by gitwatch.sh

--- src/main/java/AirportComparator.java
+++ src/main/java/AirportComparator.java
@@ -1,2 +1,13 @@
-public class AirportComparator {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class AirportComparator extends WritableComparator {
+    protected AirportComparator() {
+        super(AirportWritable.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable a, WritableComparable b){
+        return (((AirportWritable)a).getAirport_ID() - ((AirportWritable)b).getAirport_ID());
+    }
 }
