d84363caedc9ed5cb72637cb67477839ee729146
Lanak <lanak-mu@mail.ru>
2020-11-16 01:31:54 +0300

    Scripted auto-commit on change (2020-11-16 01:31:54) by gitwatch.sh

--- src/main/java/JoinClass.java
+++ src/main/java/JoinClass.java
@@ -24,8 +24,8 @@ public class JoinClass {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setPartitionerClass(TextPair.FirstPartitioner.class);
-        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setPartitionerClass(AirportPartitioner.class);
+        job.setGroupingComparatorClass(AirportComparator.class);
         job.setReducerClass(JoinRedu.class);
         job.setMapOutputKeyClass(AirportWritable.class);
         job.setOutputKeyClass(Text.class);
