8457b75498a04d4e572df98334e8dfb0dcecfc05
Lanak <lanak-mu@mail.ru>
2020-12-07 22:53:16 +0300

    Scripted auto-commit on change (2020-12-07 22:53:16) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-public class main {
-    SparkConf conf = new SparkConf().setAppName("lab3");
-    JavaSparkContext sc = new JavaSparkContext(conf);
-    
+public class Main {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> flights = sc.textFile(args[0]);
+        JavaRDD<String> airports = sc.textFile(args[1]);
+        
+    }
 
 }
