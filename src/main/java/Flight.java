10eb20abb934bd5544969f6a22f865bf3a8ccf44
Lanak <lanak-mu@mail.ru>
2020-12-08 04:08:53 +0300

    Scripted auto-commit on change (2020-12-08 04:08:53) by gitwatch.sh

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,7 +1,30 @@
-public class Flight {
+import java.io.Serializable;
+
+public class Flight implements Serializable {
     private String airportOfDeparture;
     private String airportOfArrival;
     private boolean isCancelled;
     private int timeOfDelay;
+    Flight(String airportOfArrival,String airportOfDeparture, boolean isCancelled, int timeOfDelay){
+        this.airportOfArrival = airportOfArrival;
+        this.airportOfDeparture = airportOfDeparture;
+        this.isCancelled = isCancelled;
+        this.timeOfDelay = timeOfDelay;
+    }
 
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    public int getTimeOfDelay() {
+        return timeOfDelay;
+    }
+
+    public String getAirportOfArrival() {
+        return airportOfArrival;
+    }
+
+    public String getAirportOfDeparture() {
+        return airportOfDeparture;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +10,17 @@ public class Main {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> flights = sc.textFile(args[0]);
         JavaRDD<String> airports = sc.textFile(args[1]);
-
+        JavaRDD<String> finalFlights = flights;
+        JavaRDD<String> finalAirports = airports;
+        flights = flights.filter(a -> !a.equals(finalFlights.first()));
+        airports = airports.filter(a -> !a.equals(finalAirports.first()));
+        JavaPairRDD<String,String> airport = airports.mapToPair(
+                line -> {
+                String[] columns = line.split(",");
+                String code = columns[0].replace("\"","");
+                String description = columns[1].replace("\"","");
+                return new Tuple2<>(code,description);
+                });
     }
 
 }
