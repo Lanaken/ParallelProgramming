728f53343044e2532e53a70bba8438b6f0ba13bf
Lanak <lanak-mu@mail.ru>
2020-12-09 02:29:35 +0300

    Scripted auto-commit on change (2020-12-09 02:29:35) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,12 +43,7 @@ public class Main {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3").setMaster("local[2]");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        flights = sc.textFile(args[0]);
-        airports = sc.textFile(args[1]);
-        JavaRDD<String> finalFlights = flights;
-        JavaRDD<String> finalAirports = airports;
-        flights = flights.filter(a -> !a.equals(finalFlights.first()));
-        airports = airports.filter(a -> !a.equals(finalAirports.first()));
+        downloadData(sc,args);
         makePairRDD();
         final Broadcast<Map<String,String>> airpotsBroadcast = sc.broadcast(airport.collectAsMap());
         flight.groupByKey().mapValues(
