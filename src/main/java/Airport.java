75400c0c12c1483c153a716ddd9854bdb420ad50
Lanak <lanak-mu@mail.ru>
2020-12-08 04:14:46 +0300

    Scripted auto-commit on change (2020-12-08 04:14:46) by gitwatch.sh

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -2,4 +2,15 @@
 import scala.Serializable;
 
 public class Airport implements Serializable {
+    private int code = 0;
+    private int description = 1;
+    private String[] airport;
+
+    public int getCode() {
+        return code;
+    }
+
+    public int getDescription() {
+        return description;
+    }
 }
