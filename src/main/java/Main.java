8cc608260ce8c3bc4549679f19d54eeec4e405c1
Lanak <lanak-mu@mail.ru>
2020-12-09 21:27:00 +0300

    Scripted auto-commit on change (2020-12-09 21:27:00) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -46,7 +46,7 @@ public class Main {
                     String[] columns = line.split(",");
                     boolean cancelled = !columns[19].isEmpty();
                     String departure = columns[11];
-                    //System.out.println(columns[19].isEmpty());
+                    System.out.println(columns[19]);
                     String destination = columns[14];
                     float timeOfDelay = columns[18].isEmpty() ? 0 : Float.parseFloat(columns[18]);
                     return new Tuple2<>(new Tuple2<>(departure,destination),new Flight(destination,departure,cancelled,timeOfDelay));
