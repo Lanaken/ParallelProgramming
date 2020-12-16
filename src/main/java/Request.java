d1a00f96633684351dac845235d195bed9adcdcf
Lanak <lanak-mu@mail.ru>
2020-12-16 18:31:44 +0300

    Scripted auto-commit on change (2020-12-16 18:31:44) by gitwatch.sh

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -6,4 +9,9 @@ public class Request implements Serializable {
     private String jsScript;
     private String functionName;
     private ArrayList<Test> tests;
+
+    @JsonCreator
+    Request(@JsonProperty("packageId")String packageId,@JsonProperty("jsScript")String jsScript,@JsonProperty("functionName") String functionName){
+
+    }
 }
