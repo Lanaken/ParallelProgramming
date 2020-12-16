2b1aa1d64aff439cc8dbf143f517ba5be4e5868e
Lanak <lanak-mu@mail.ru>
2020-12-16 19:14:36 +0300

    Scripted auto-commit on change (2020-12-16 19:14:36) by gitwatch.sh

--- src/main/java/ResultActor.java
+++ src/main/java/ResultActor.java
@@ -1,6 +1,8 @@
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 
+import javax.script.ScriptEngine;
+
 public class ResultActor extends AbstractActor {
     private ActorRef storage;
 
@@ -9,7 +11,7 @@ public class ResultActor extends AbstractActor {
     }
 
     private String runTest(){
-        
+        ScriptEngine
     }
 
     @Override
