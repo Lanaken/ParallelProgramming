422c843017de076ebfad6848b13130ead55aeb19
Lanak <lanak-mu@mail.ru>
2020-12-16 18:28:35 +0300

    Scripted auto-commit on change (2020-12-16 18:28:35) by gitwatch.sh

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,7 +1,9 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Request implements Serializable {
     private String packageId;
     private String jsScript;
     private String functionName;
+    private ArrayList<Test> tests;
 }
