6e470651dec5d6e54f3fa9521d5ced27114d290c
Lanak <lanak-mu@mail.ru>
2020-12-16 19:40:13 +0300

    Scripted auto-commit on change (2020-12-16 19:40:13) by gitwatch.sh

--- src/main/java/ResultActor.java
+++ src/main/java/ResultActor.java
@@ -29,6 +29,6 @@ public class ResultActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().match(Test.class, func -> storage.tell(check(func)), ActorRef.noSender());
+        return ReceiveBuilder.create().match(Test.class, func -> storage.tell(check(func), ActorRef.noSender()));
     }
 }
