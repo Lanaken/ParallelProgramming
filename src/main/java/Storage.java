67cc78cb37772b6f8e72e6b696781d34aa3a0797
Lanak <lanak-mu@mail.ru>
2020-12-16 20:31:19 +0300

    Scripted auto-commit on change (2020-12-16 20:31:19) by gitwatch.sh

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,7 +1,7 @@
 import akka.actor.AbstractActor;
 
 public class Storage extends AbstractActor {
-    private 
+    private Map<>
     @Override
     public Receive createReceive() {
         return null;
