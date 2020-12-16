b83d2a35e8a08a204a094339a6caddcb0b921a71
Lanak <lanak-mu@mail.ru>
2020-12-16 19:50:33 +0300

    Scripted auto-commit on change (2020-12-16 19:50:33) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -10,7 +10,7 @@ public class Router extends AbstractActor {
     private SupervisorStrategy supervisorStrategy;
 
     Router(ActorSystem actorSystem){
-        storageActor = actorSystem.actorOf(Prop.create(S))
+        storageActor = actorSystem.actorOf(Props.create(S))
     }
 
 
