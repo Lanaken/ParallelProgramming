b47702d93d63fa3556c3360867004e1b8e3ef444
Lanak <lanak-mu@mail.ru>
2020-12-16 17:36:54 +0300

    Scripted auto-commit on change (2020-12-16 17:36:54) by gitwatch.sh

---
+++ src/main/java/AkkaApp.java
@@ -0,0 +1,9 @@
+import akka.actor.ActorSystem;
+import akka.actor.ActorSystemImpl;
+
+public class AkkaApp {
+    public static void main(String[] args){
+        ActorSystem actorSystem = ActorSystem.create("AkkaApp");
+        
+    }
+}
