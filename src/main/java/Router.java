b762e90d376184331787f72b13bcddeee7026bec
Lanak <lanak-mu@mail.ru>
2020-12-16 19:45:15 +0300

    Scripted auto-commit on change (2020-12-16 19:45:15) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,2 +1,8 @@
-public class Router {
+import akka.actor.AbstractActor;
+
+public class Router extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
