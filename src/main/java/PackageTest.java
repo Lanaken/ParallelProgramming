12aecaf12f3e3d2d6c58a3ff96bfb1f8f52dbe38
Lanak <lanak-mu@mail.ru>
2020-12-20 05:02:36 +0300

    Scripted auto-commit on change (2020-12-20 05:02:36) by gitwatch.sh

--- src/main/java/PackageTest.java
+++ src/main/java/PackageTest.java
@@ -1,2 +1,20 @@
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "packageID",
+        "jsScript",
+        "functionName",
+        "tests"
+})
 public class PackageTest {
+  @JsonProperty("packageID") private String packageID;
+  @JsonProperty("jsScript") private String jsScript;
+  @JsonProperty("packageID") private String functionName;
+  @JsonProperty("packageID") private List<> tests;
+
 }
