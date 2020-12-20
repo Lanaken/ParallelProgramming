91f09732ab8ce23ac29c71faa7f05a2feb618119
Lanak <lanak-mu@mail.ru>
2020-12-20 04:27:30 +0300

    Scripted auto-commit on change (2020-12-20 04:27:30) by gitwatch.sh

--- src/main/java/SingleResult.java
+++ src/main/java/SingleResult.java
@@ -1,2 +1,17 @@
-public class SingleResult {
+]public class SingleResult {
+    private String packageId;
+    private String result;
+
+    public SingleResult(String packageId, String result){
+        this.packageId = packageId;
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
 }
