98f9c84fbc93dca69004036d3a6de89c74bc8881
Lanak <lanak-mu@mail.ru>
2020-12-20 04:33:57 +0300

    Scripted auto-commit on change (2020-12-20 04:33:57) by gitwatch.sh

--- src/main/java/PackageResult.java
+++ src/main/java/PackageResult.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class PackageResult {
+    private String packageID;
+    private ArrayList<String> results;
+
+    PackageResult(String packageID,ArrayList<String> results){
+        this.packageID = packageID;
+        this.results = results;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
+
+    public ArrayList<String> getResults() {
+        return results;
+    }
 }
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,5 @@
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 import com.sun.org.apache.xml.internal.security.utils.SignatureElementProxy;
 
 import java.util.ArrayList;
@@ -6,20 +7,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Storage extends AbstractActor {
-    private Map<String, ArrayList<Test>> store = new HashMap<>();
+    private Map<String, ArrayList<String>> store = new HashMap<>();
 
-    private void input(Test test){
-        String packageId = test.getParent().getPackageId();
-        if (store.containsKey(packageId))
-            store.get(packageId).add(test);
-        else{
-            ArrayList<Test> tests = new ArrayList<>();
-            tests.add(test);
-            store.put(packageId,tests);
-        }
-    }
     @Override
     public Receive createReceive() {
-        return receiveBuilder().match(Test.class, test -> input(test)).match(String.class, id -> sender().tell())
+        return ReceiveBuilder.create().match(SingleResult.class, this::g)
     }
 }
