59e40004471f798b74fd0f9277e44f37cad5943f
Lanak <lanak-mu@mail.ru>
2020-12-20 04:26:18 +0300

    Scripted auto-commit on change (2020-12-20 04:26:18) by gitwatch.sh

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,2 +1,20 @@
-public class Result {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Result implements Serializable {
+    private String packageID;
+    private ArrayList<String> results;
+
+    public Result(String packageID,ArrayList<String> results){
+        this.packageID = packageID;
+        this.results = results;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
+
+    public ArrayList<String> getResults() {
+        return results;
+    }
 }
