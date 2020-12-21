1e6ac8048281b7da98883c8ff5babd7dab1d545c
Lanak <lanak-mu@mail.ru>
2020-12-22 01:08:35 +0300

    Scripted auto-commit on change (2020-12-22 01:08:35) by gitwatch.sh

--- Lab5/src/main/java/GetResult.java
+++ Lab5/src/main/java/GetResult.java
@@ -1,2 +1,17 @@
 public class GetResult {
+    private String url;
+    private int count;
+
+    public GetResult(String url,int count){
+        this.count = count;
+        this.url = url;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
