da2d4839e8861c7c7be6b3ff4ffaf6d329664a73
Lanak <lanak-mu@mail.ru>
2020-12-22 01:01:30 +0300

    Scripted auto-commit on change (2020-12-22 01:01:30) by gitwatch.sh

--- Lab5/src/main/java/Actor.java
+++ Lab5/src/main/java/Actor.java
@@ -1,2 +1,13 @@
-public class Actor {
+import akka.actor.AbstractActor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Actor extends AbstractActor {
+    private Map<String,Long> storage = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
