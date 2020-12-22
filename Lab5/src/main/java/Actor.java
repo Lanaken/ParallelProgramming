264a019da1d58927c57a0fa566ffc87d8d5f4eea
Lanak <lanak-mu@mail.ru>
2020-12-22 20:39:22 +0300

    Scripted auto-commit on change (2020-12-22 20:39:22) by gitwatch.sh

--- Lab5/src/main/java/Actor.java
+++ Lab5/src/main/java/Actor.java
@@ -12,6 +12,7 @@ public class Actor extends AbstractActor {
         return ReceiveBuilder.create().match(GetResult.class, msg -> {
             boolean isCounted = storage.containsKey(msg.getUrl());
             String url = msg.getUrl();
+            System.out.println(url);
             if (isCounted){
                 sender().tell(new ResponseResult(isCounted,url,storage.get(url)),getSelf());
             }
