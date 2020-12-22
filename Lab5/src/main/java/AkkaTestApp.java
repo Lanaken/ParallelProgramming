d57a02cfc8f29e7f25f26bce06276fa50592ccba
Lanak <lanak-mu@mail.ru>
2020-12-22 05:41:03 +0300

    Scripted auto-commit on change (2020-12-22 05:41:03) by gitwatch.sh

--- Lab5/src/main/java/AkkaTestApp.java
+++ Lab5/src/main/java/AkkaTestApp.java
@@ -10,7 +10,10 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
+import org.asynchttpclient.AsyncHttpClient;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -19,6 +22,8 @@ import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.regex.Pattern;
+
+import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class AkkaTestApp {
     public static void main(String[] args) throws IOException {
@@ -33,15 +38,25 @@ public class AkkaTestApp {
                     String url = request.getUri().query().get("testUrl").orElse("http://bmstu.ru");
                     Integer count = Integer.parseInt(request.getUri().query().get("count").orElse("1"));
                     return new GetResult(url,count);
-                }).mapAsync(4, param -> {
+                }).mapAsync(4, param ->
                     Patterns.ask(storeRef,param, Duration.ofMillis(5000))
                             .thenCompose(msg -> {
                                 ResponseResult responseResult = (ResponseResult) msg;
                                 if (responseResult.isEmpty())
                                     return CompletableFuture.completedFuture(responseResult);
-                            }
-                            return Source.from(Collections.singletonList(param)))
-                })
+
+                            return Source.from(Collections.singletonList(param))
+                                    .toMat(testSink(),Keep.right()).run(materializer)
+                                    .thenCompose(time -> CompletableFuture.completedFuture(
+                                            new ResponseResult(false,param.getUrl(),time/param.getCount())));
+                }))
+                .map(response -> {
+                    if (!response.isEmpty()){
+                  StoreResult storeResult = new StoreResult(response.getUrl(),response.getTime());
+                  storeRef.tell(storeResult,ActorRef.noSender());
+                }
+                    return HttpResponse.create().withStatus(200).withEntity(response.getTime() + "ms");
+                });
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
@@ -51,5 +66,20 @@ public class AkkaTestApp {
         System.in.read();
         binding.thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
+    }
+
+    static final Sink<GetResult,CompletionStage<Long>> testSink(){
+        return Flow.<GetResult>create()
+                .mapConcat(msg -> Collections.nCopies(msg.getCount(),msg.getUrl()))
+                .mapAsync(4, pair -> {
+                    Long startTime = System.currentTimeMillis();
+                    AsyncHttpClient asyncHttpClient = asyncHttpClient();
+                    return asyncHttpClient
+                            .prepareGet(pair)
+                            .execute()
+                            .toCompletableFuture()
+                            .thenCompose(response ->
+                                CompletableFuture.completedFuture(System.currentTimeMillis() - startTime));
+                }).toMat(Sink.fold(0L,Long::sum), Keep.right());
     }
 }
